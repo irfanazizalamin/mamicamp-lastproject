b5a48040181a3d3edcb42a674f5c23cf1dd00f1b
Irfan Aziz Al Amin <irfanaziz2929@gmail.com>
2020-06-06 09:26:48 +0700

    return list thread without link

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-full">
+    <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads"/>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data'
+import ThreadList from './ThreadList'
+console.log(Object.values(sourceData.threads))
+export default {
+  name: 'HelloWorld',
+  components: {
+    ThreadList
+  },
+  data () {
+    return {
+      threads: Object.values(sourceData.threads),
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
